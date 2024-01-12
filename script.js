bfd5bc3e90d5afef78d70fe300abe36a305c5379
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-01-12 22:23:39 +0530

    logo

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const colorArray = [
   ['#CF5C36', '#050517', '#ffffff', '#ffffff'],
   ['#F7717D', '#16001E', '#ffffff', '#ffffff'],
   ['#FFE66D', '#2F3061', '#F7FFF7', '#343434'],
-  ['#161A1E', '#22272C', '#FAF9F6', '#FAF9F6'], //white and black
+  // ['#161A1E', '#22272C', '#FAF9F6', '#FAF9F6'], //white and black
 //   ['#0d0d0d', '#FAF9F6', '#0d0d0d', '#FAF9F6'], //white and black
   ['#33b15d', '#0d0d0d', '#ffffff', '#ffffff'],
 
