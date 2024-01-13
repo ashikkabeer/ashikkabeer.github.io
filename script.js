41cba2847233ff49b40d6a00c40b326b3c5c56dd
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-01-13 09:33:43 +0530

    color change

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const colorArray = [
   //replacce [green, black, text-color-white, ashiktext]
   ['#CF5C36', '#050517', '#ffffff', '#ffffff'],
   ['#F7717D', '#16001E', '#ffffff', '#ffffff'],
-  ['#FFE66D', '#2F3061', '#F7FFF7', '#343434'],
+  ['#FFE66D', '#0d0d0d', '#F7FFF7', '#343434'],
   // ['#161A1E', '#22272C', '#FAF9F6', '#FAF9F6'], //white and black
 //   ['#0d0d0d', '#FAF9F6', '#0d0d0d', '#FAF9F6'], //white and black
   ['#33b15d', '#0d0d0d', '#ffffff', '#ffffff'],
