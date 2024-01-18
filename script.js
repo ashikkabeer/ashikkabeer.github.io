ccae74183da0a16507494456053dc009d050799c
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-01-18 09:52:56 +0530

    colors removed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const colorArray = [
   //replacce [green, black, text-color-white, ashiktext]
-  ['#CF5C36', '#050517', '#ffffff', '#ffffff'],
-  ['#F7717D', '#16001E', '#ffffff', '#ffffff'],
+  // ['#CF5C36', '#050517', '#ffffff', '#ffffff'],
+  // ['#F7717D', '#16001E', '#ffffff', '#ffffff'],
   ['#FFE66D', '#0d0d0d', '#F7FFF7', '#343434'],
   // ['#161A1E', '#22272C', '#FAF9F6', '#FAF9F6'], //white and black
   ['#FAF9F6', '#0d0d0d', '#FAF9F6', '#0d0d0d'], //white and black
@@ -30,10 +30,8 @@ function changeColors() {
 
   // Update the CSS variables
   document.documentElement.style.setProperty('--primary-color', primaryColor);
-  document.documentElement.style.setProperty(
-    '--secondary-color',
-    secondaryColor
-  );
+  document.documentElement.style.setProperty('--secondary-color',secondaryColor);
   document.documentElement.style.setProperty('--text-color', textColor);
   document.documentElement.style.setProperty('--hero-text', heroText);
 }
+
