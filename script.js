2320aa2ab038b0d8b7bed74970bd4feef52ed484
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-03-12 06:44:46 +0530

    removed nav

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const colorArray = [
   ['#FFE66D', '#0d0d0d', '#F7FFF7', '#343434'],
   // ['#161A1E', '#22272C', '#FAF9F6', '#FAF9F6'], //white and black
   ['#FAF9F6', '#0d0d0d', '#FAF9F6', '#0d0d0d'], //white and black
-  ['#33b15d', '#0d0d0d', '#ffffff', '#ffffff'],
+  ['#33b15d', '#0d0d0d', '##FAF9F6', '##FAF9F6'],
 
 
   // Add more color combinations as needed
